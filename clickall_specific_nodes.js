0e9aa46b99c5ec4e54b7fd2e444950004147fb67
lostpackets <119964688+lostpackets@users.noreply.github.com>
2023-01-21 03:25:16 +0000

    Update clickall_specific_nodes.js

--- clickall_specific_nodes.js
+++ clickall_specific_nodes.js
@@ -4,3 +4,10 @@ document.querySelectorAll("button").forEach( function(element) {
 });
 //Clicks a node (first node)
 document.querySelector("button").dispatchEvent(new Event("click"));
+//Click by array of node
+var buttons = document.querySelectorAll("button");
+buttons[2].dispatchEvent(new Event("click"));
+//       \------- Selects 3rd element, as it starts from 0 like an array
+//
+//Like the previous command, but it uses CSS:
+document.querySelector("button:nth-child(3)").dispatchEvent(new Event("click"));
